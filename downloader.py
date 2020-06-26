767befc68e563ef17f53975d708112fec3d76f1b
jbernadas <jb3rns@gmail.com>
2020-06-26 16:39:01 -0700

    Added tar and tar.gz to downloader, but commented out

--- downloader.py
+++ downloader.py
@@ -7,28 +7,20 @@ url = input("What is the page you want to download from? ")
 
 folder_location = './docs_for_upload'
 
-# html_page = requests.get(download_page).text
-
-# initial_soup = BeautifulSoup(html_page, "lxml")
-
-# soup = initial_soup.find('div', attrs={'class': 'content'})
-
-# current_link = ''
-
-# doc_ends = ['.pdf', '.docx', 'txt', 'doc', 'wrf', 'xls', 'xlsx']
-
-# for link in soup.find_all('a'):
-#     current_link = link.get('href')
-#     # if current_link.endswith()
-#     # for doc_end in doc_ends:
-#     #     if current_link.endswith(doc_end):
-#     print('I found a doc: ' + current_link)
-
 response = requests.get(url).text
 soup = BeautifulSoup(response, "lxml")
 
-QUALIFIERS = ["a[href$='.pdf']", "a[href$='.doc']", "a[href$='.docx']", "a[href$='.txt']",
-              "a[href$='.wrf']", "a[href$='.xls']", "a[href$='.xlsx']"]
+QUALIFIERS = [
+    "a[href$='.pdf']",
+    "a[href$='.doc']",
+    "a[href$='.docx']",
+    "a[href$='.txt']",
+    "a[href$='.wrf']",
+    "a[href$='.xls']",
+    "a[href$='.xlsx']"
+    # "a[href$='.tar']",
+    # "a[href$='.tar.gz']"
+]
 
 for qualifier in QUALIFIERS:
     for link in soup.select(qualifier):
