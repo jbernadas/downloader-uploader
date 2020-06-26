ea898ec9ee707fb4765a05293e69d4b9855a6d55
jbernadas <jb3rns@gmail.com>
2020-06-26 12:00:05 -0700

    Success!

--- pdf-uploader.py
+++ pdf-uploader.py
@@ -26,15 +26,15 @@ driver.get(target_site + '/login')
 proceed = input(
     "Are you logged-in and ready to proceed? 'y' = yes, any key to abort: ")
 
-FILESDIR = "./files_for_upload"
+FILESDIR = "files_for_upload"
 
 if proceed == 'y':
-    for filename in os.listdir(FILESDIR):
+    for filename in os.listdir('./' + FILESDIR):
         if filename.endswith('.pdf'):
             wait = WebDriverWait(driver, 60)
             driver.get(target_site + "/media/add/document")
             driver.find_element_by_id(
-                "edit-field-document-0-upload").send_keys(os.path.join(FILESDIR, filename))
+                "edit-field-document-0-upload").send_keys(os.getcwd() + '\\' + FILESDIR + '\\' + filename)
             wait.until(presence_of_element_located(
                 (By.NAME, 'field_document_0_remove_button')))
             driver.find_element(
@@ -42,5 +42,6 @@ if proceed == 'y':
             wait.until(element_to_be_clickable(
                 (By.XPATH, 'html/body/div[2]/div[1]/main/div[4]/div[1]/form/div[8]/input[@id="edit-submit"]'))).click()
             continue
+    driver.quit()
 else:
     driver.quit()
